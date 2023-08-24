650a02f4f49a28b6c45d05f03d47c4813ebf91be
Oliver Sim <choo-howe.sim@edag.com.my>
2023-08-25 00:02:35 +0800

    ci: fix heroku deployment

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/'));
 
-app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', {root: 'dist/'});
 });
 
 // Start the app by listening on the default Heroku port

0e55fd6768dc0df49c4e0140d1a37cb4bf96d801
Oliver Sim <choo-howe.sim@edag.com.my>
2023-08-25 13:38:03 +0800

    ci: fix outputPath

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fire-apps'),
+      dir: require('path').join(__dirname, './coverage/futbuddy-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
